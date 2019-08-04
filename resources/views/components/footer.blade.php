17f1f6cc5bafebb1b96486bd10af279b6c07d6c5
Kento-Ishizaki <kentoron1115@gmail.com>
2019-08-04 15:52:53 +0900

    トップページ コンテンツ追加

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="py-5 bg-dark mt-3">
     <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Map Date</p>
+        <p class="m-0 text-center text-white">Copyright &copy; Gmap App</p>
     </div>
     <!-- /.container -->
 </footer>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,76 +1,65 @@
 @extends('layout')
 
 @section('content')
-  <!-- Page Content -->
-  <div class="container">
+    <!-- Page Content -->
+    <div class="container-fruid">
 
-    <!-- Jumbotron Header -->
-    <div class="jumbotron my-4 border-warning top-image text-black">
-        <div class="bg-mask">
-            <h1 class="display-1">Gmap App</h1>
-            <p class="h3">グーグルマップAPIを使った予定共有アプリです。</p>
-            <a href="{{ route('map.index') }}" class="btn btn-warning btn-lg">マップを見る</a>
+        <!-- Jumbotron Header -->
+        <div class="jumbotron mb-4 border-warning top-image">
+            <div class="bg-mask">
+                <h1 class="display-1">Gmap App</h1>
+                <p class="h3">グーグルマップAPIを使った予定共有アプリです。</p>
+                <a href="{{ route('map.index') }}" class="btn btn-warning btn-lg">マップを見る</a>
+            </div>
         </div>
     </div>
+    <!-- ./container-fluid -->
 
     <!-- Page Features -->
-    <div class="row text-center">
+    <div class="container">
+        <div class="row text-center">
 
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100 border-warning">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-outline-warning">Find Out More!</a>
-          </div>
+        <div class="col-lg-4 mb-4">
+            <div class="card h-100 border-primary">
+            <img class="card-img-top card-header" src="{{ asset('test-user.png') }}" alt="">
+            <div class="card-body">
+                <h4 class="card-title">まずはみんなの予定を確認</h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn-outline-warning">Find Out More!</a>
+            </div>
+            </div>
         </div>
-      </div>
 
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100 border-warning">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-outline-warning">Find Out More!</a>
-          </div>
+        <div class="col-lg-4 mb-4">
+            <div class="card h-100 border-primary">
+            <img class="card-img-top card-header" src="{{ asset('test-user.png') }}" alt="">
+            <div class="card-body">
+                <h4 class="card-title">気になる予定があったらコメントして見ましょう</h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn-outline-warning">Find Out More!</a>
+            </div>
+            </div>
         </div>
-      </div>
 
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100 border-warning">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-outline-warning">Find Out More!</a>
-          </div>
+        <div class="col-lg-4 mb-4">
+            <div class="card h-100 border-primary">
+            <img class="card-img-top card-header" src="{{ asset('test-user.png') }}" alt="">
+            <div class="card-body">
+                <h4 class="card-title">テストユーザーでもログイン可能です</h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn-outline-warning">Find Out More!</a>
+            </div>
+            </div>
         </div>
-      </div>
 
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100 border-warning">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-outline-warning">Find Out More!</a>
-          </div>
         </div>
-      </div>
-
+        <!-- /.row -->
     </div>
-    <!-- /.row -->
-
-  </div>
-  <!-- /.container -->
+    <!-- /.container -->
 @endsection
